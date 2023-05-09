f7f6af8c126b39c0a397e956497667bd348b7d4b
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-05-08 21:01:26 -0400

    added CRUD operators in hatsController

--- controllers/hatsController.js
+++ controllers/hatsController.js
@@ -1,6 +1,6 @@
 // const express = require('express');
 // const hats = express.Router();
-// const hatsController = require("./hatsController"); 
+
 
 // const {
 //     getAllHats,
@@ -10,5 +10,48 @@
 //     deleteHat
 // } = require("../queries/hats"); 
 
+
+
 // hats.get("/", async (req, res) => {
 //     const allHats = await getAllHats();
+//    res.json(allHats);
+// });
+
+
+// hats.get("/:id", async (req,res)) => {
+// const {id} = req.params; 
+// const hat = await getHatById(id);
+// res.json(hat);
+// };
+
+
+
+// hats.post("/", async (req, res) => {
+    // const newHat = await createHat(req.body); 
+    // res.json(newHat);
+// });  
+
+
+
+// hats.put("/:id", async (req, res)) => {
+//     const {id} = req.params;
+// const updatedHat = await updateHat(id, req.body);
+// res.json(updatedHat);
+// };   
+
+
+
+// hats.delete("/:id", async (req, res)) => {
+//     const {id} = req.params;
+// const deletedHat = await deleteHat(id);
+// res.json(deletedHat);
+// };
+
+
+// module.exports = hats;
+
+
+
+
+
+
